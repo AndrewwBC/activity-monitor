fb21e7051b932751f682a94dd5124f688125ceba
AndrewwBC <andrew-hot2011@hotmail.com>
2025-03-24 19:54:15 -0300

    fix: background js

--- src/extensionJS/background.js
+++ src/extensionJS/background.js
@@ -1,6 +1,44 @@
-chrome.browserAction.onClicked.addListener(function (tab) {
-  chrome.tabs.query({}, function (tabs) {
-    let titles = tabs.map((tab) => tab.title);
-    console.log(titles);
-  });
+var tabsIds = {};
+var urlList = [];
+
+function isInvalidUrl(url) {
+  console.log(url);
+  const invalidUrls = [
+    "",
+    "about:blank",
+    "chrome://newtab",
+    "chrome://extensions",
+  ];
+
+  if (invalidUrls.includes(url) || url == undefined) return true;
+  else return false;
+}
+
+chrome.tabs.onUpdated.addListener((tabid, changeInfo, tab) => {
+  console.log(tabid, tab, changeInfo);
+  if (isInvalidUrl(tab.url)) return;
+  tabsIds[tab.title] = tab.id;
+  console.log("Url util");
+});
+
+chrome.tabs.onCreated.addListener((tabid, changeInfo, tab) => {
+  console.log(tabid, tab, changeInfo);
+  if (isInvalidUrl(tab.url)) {
+    return;
+  }
+
+  tabsIds[tab.title] = tab.id;
+  console.log(`Titulo: ${tab.title} e Id: ${tab.id}`);
+
+  // getting the url in order to match the tab title
+  let cleanUrl = tab.url.match(/https?:\/\/[^\/]+\/?/)[0];
+
+  urlList.push(cleanUrl);
+});
+
+chrome.tabs.onRemoved.addListener(function (tabId) {
+  let tabTitle = tabsIds[tabId];
+  console.log(tabTitle);
+
+  if (tabTitle) openTabs = openTabs.filter((tab) => tab !== tabTitle);
 });
--- src/extensionJS/script.js
+++ src/extensionJS/script.js
@@ -1,11 +1,27 @@
-document.addEventListener("DOMContentLoaded", function () {
-  chrome.tabs.query({}, function (tabs) {
-    let titlesList = document.getElementById("titlesList");
+let titlesList = document.getElementById("titlesList");
+var urlList = [];
+var openTabs = [];
 
-    tabs.forEach((tab) => {
-      let listItem = document.createElement("li");
-      listItem.textContent = tab.title;
-      titlesList.appendChild(listItem);
-    });
-  });
+let tabsIds = {};
+
+chrome.tabs.onCreated.addListener({}, (tab) => {
+  tabsIds[tab.title] = tab.id;
+  console.log(tab.title);
+
+  let listItem = document.createElement("li");
+  listItem.textContent = tab.title;
+  titlesList.appendChild(listItem);
+
+  // getting the url in order to match the tab title
+  let cleanUrl = tab.url.match(/https?:\/\/[^\/]+\/?/)[0];
+  openTabs.push(tab.title);
+  urlList.push(cleanUrl);
 });
+
+chrome.tabs.onRemoved.addListener(function (tabId, removeInfo) {
+  let tabTitle = openTabs[tabId];
+
+  if (tabTitle) openTabs = openTabs.filter((tab) => tab !== tabTitle);
+});
+
+chrome.tabs.onRemove;
